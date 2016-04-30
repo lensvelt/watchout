d2561a4ee6a5b25aa8c961eb313b4d4bc588da4e
lensvelt <lensvelt.brett@gmail.com>
2016-04-29 17:28:48 -0700

    Implement basic dragging behavior for protagonist

--- client/watchout.js
+++ client/watchout.js
@@ -15,20 +15,7 @@ var values = [{id: 1, x: 10, y: 10},
               {id: 2, x: 20, y: 20}, 
               {id: 3, x: 30, y: 30}, 
               {id: 4, x: 40, y: 40}];
-
-var heroNode = svg.selectAll('circle').data(heroVal, function(d) { return d.id; })
-             .enter().append('ellipse')
-             .attr('cx', function(d, i) { return d.x; })
-             .attr('cy', function(d, i) { return d.y; })
-             .attr('rx', 15)
-             .attr('ry', 25); 
-
-var nodes = svg.selectAll('circle').data(values, function(d) { return d.id; })
-              .enter().append('circle')
-              .attr('cx', function(d, i) { return d.x; })
-              .attr('cy', function(d, i) { return d.y; })
-              .attr('r', 25);
-
+              
 var drag = d3.behavior.drag()
              .on('drag', function(d, i) { 
                d.x += d3.event.dx;
@@ -37,6 +24,30 @@ var drag = d3.behavior.drag()
                  return 'translate(' + [d.x, d.y] + ')';
                });
              });
+
+var heroNode = svg.selectAll('circle').data(heroVal, function(d) { return d.id; })
+             .enter().append('ellipse')
+             .attr('cx', function(d, i) { return d.x; })
+             .attr('cy', function(d, i) { return d.y; })
+             .attr('rx', 15)
+             .attr('ry', 25)
+             .call(drag);
+
+var nodes = svg.selectAll('circle').data(values, function(d) { return d.id; })
+              .enter().append('circle')
+              .attr('cx', function(d, i) { return d.x; })
+              .attr('cy', function(d, i) { return d.y; })
+              .attr('r', 25);
+
+// var initialize = function() {
+//   // generate ids
+//   var count = 0;
+
+//   // set starting points for hero
+  
+//   // set starting points for enemies
+// }
+
 // randomize enemy placement
 var relocate = function (val) {
   val.x = Math.floor(w * Math.random());
